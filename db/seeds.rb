1e777484758ead593187d62bd2c11034a6a8b52c
Jaume Ros <jaumeros77@gmail.com>
2022-08-24 13:09:47 +0200

    i hate Trip Adv

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,29 @@
 
 # ENV["KEY"]
 
-response = RestClient.get("https://opendata.aemet.es/opendata/api/prediccion/maritima/costera/costa/42?api_key=#{ENV["AEMET_KEY"]}")
-coast_array = JSON.parse(response)
 
-actual_link = RestClient.get(coast_array["datos"])
-actual_array_link = JSON.parse(actual_link)
+# actual_link = RestClient.get(coast_array["datos"])
+# actual_array_link = JSON.parse(actual_link)
 
-actual_array_link.each do |water|
-    binding.pry
-end 
+# actual_array_link.each do |water|
+#     binding.pry
+# end 
 
+
+# response = RestClient.get("https://opendata.aemet.es/opendata/api/prediccion/maritima/costera/costa/42?api_key=#{ENV["AEMET_KEY"]}")
+# coast_array = JSON.parse(response)
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+url = URI("https://api.content.tripadvisor.com/api/v1/location/search?searchQuery=SantCugat&category=%22restaurants%22&language=en&key=586AFE7C1934478AB8D2FBF09FD73AEB")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["Accept"] = 'application/json'
+
+response = http.request(request)
+puts response.read_body
