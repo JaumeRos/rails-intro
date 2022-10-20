e28cb3e00d6617b3be583429908f115e0fb80f4d
Jaume Ros <jaumeros77@gmail.com>
2022-10-20 16:02:07 +0200

    not sure but need to get back into it

--- db/migrate/20221010121919_add_french_hotel_to_french_city.rb
+++ db/migrate/20221010121919_add_french_hotel_to_french_city.rb
@@ -1,5 +1,5 @@
 class AddFrenchHotelToFrenchCity < ActiveRecord::Migration[7.0]
   def change
-    add_reference :french_cities, :french_hotels, null: false, foreign_key: true
+    # add_reference :french_cities, :french_hotels, null: false, foreign_key: true
   end
 end
